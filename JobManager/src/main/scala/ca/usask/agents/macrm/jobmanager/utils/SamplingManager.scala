e677164c85641e7171824b185fc81991f7e5d3a0
MHSG <mhsedighi@gmail.com>
2015-03-05 21:41:08 -0600

    design 1.6 phase 1.3

--- JobManager/src/main/scala/ca/usask/agents/macrm/jobmanager/utils/SamplingManager.scala
+++ JobManager/src/main/scala/ca/usask/agents/macrm/jobmanager/utils/SamplingManager.scala
@@ -11,6 +11,8 @@ object SamplingManager {
     var clusterNodesWithConstraint: List[(NodeId, List[Constraint])] = null
     var clusterNodesWithoutConstraint: List[NodeId] = null
     var waveToTasks = Map[Int, List[(Boolean, TaskDescription)]]() //The Int is waveNumber
+    var nodeIdToTaskIndex = Map[NodeId, List[Int]]()
+    var completedWave = 0
 
     def loadSamplingInformation(samplingInformation: SamplingInformation) {
         samplingRate = samplingInformation.samplingRate
@@ -21,8 +23,48 @@ object SamplingManager {
     def loadNewSamplingRate(newRate: Int) = samplingRate = newRate
 
     def findSamplingListForTasksWithConstraint(SRFTT: Int, tasks: List[TaskDescription]): (Int, List[(NodeId, Resource)]) = {
-        val taskWithTheirProperNodes = tasks.map(x => (x.resource, findNodesWithProperCapabilities(SRFTT, x.constraints))).map(y => y._2.map(z => (z, y._1))).flatten
-        (taskWithTheirProperNodes.length, taskWithTheirProperNodes)
+
+        var tempNodeIdToTaskIndex = Map[NodeId, List[TaskDescription]]()
+        tasks.foreach { x =>
+            val properNodesForThisTask = findNodesWithProperCapabilities(x.constraints)
+            properNodesForThisTask.foreach(y => tempNodeIdToTaskIndex.get(y) match {
+                case None    => tempNodeIdToTaskIndex.update(y, List(x))
+                case Some(z) => tempNodeIdToTaskIndex.update(y, x :: z)
+            })
+        }
+
+        var finalResult1 = 0
+        var taskIndexToCurrSampNum = Map[Int, Int]()
+        tasks.foreach(x => taskIndexToCurrSampNum.update(x.index, 0))
+
+        var finalResult2 = List[(NodeId, Resource)]()
+        val tempNodeIdToTaskIndexList = tempNodeIdToTaskIndex.toList.sortWith((x, y) => x._2.length > y._2.length).
+            map(x => (x._1, x._2.sortWith((m, n) => m.resource < n.resource)))
+
+        tempNodeIdToTaskIndexList.foreach { x =>
+            var selected = false
+            x._2.foreach { y =>
+                if (!selected && taskIndexToCurrSampNum.get(y.index).get < SRFTT) {
+                    finalResult1 = 1 + finalResult1
+                    finalResult2 = (x._1, y.resource) :: finalResult2
+                    taskIndexToCurrSampNum.update(y.index, taskIndexToCurrSampNum.get(y.index).get + 1)
+                    selected = true
+                    nodeIdToTaskIndex.update(x._1, x._2.map(j => j.index))
+                    tempNodeIdToTaskIndex.remove(x._1)
+                }
+            }
+        }
+
+        if (finalResult1 < tasks.length * SRFTT) {
+            Random.shuffle(tempNodeIdToTaskIndex.toList).take(tasks.length * SRFTT - finalResult1).foreach { x =>
+                val sortedTasksBasedOnResource = x._2.sortWith((m, n) => m.resource < n.resource)
+                finalResult1 = 1 + finalResult1
+                finalResult2 = (x._1, sortedTasksBasedOnResource(0).resource) :: finalResult2
+                nodeIdToTaskIndex.update(x._1, sortedTasksBasedOnResource.map(y => y.index))
+            }
+        }
+
+        (finalResult1, finalResult2)
     }
 
     def getSamplingNode(tasks: List[TaskDescription], retry: Int): List[(NodeId, Resource)] = {
@@ -38,25 +80,99 @@ object SamplingManager {
         result
     }
 
-    def findNodesWithProperCapabilities(count: Int, constrints: List[Constraint]): List[NodeId] = {
+    def findNodesWithProperCapabilities(constrints: List[Constraint]): List[NodeId] = {
         val tempListOfNodeId = clusterNodesWithConstraint.filter(x => doesAllCapablitiesMatchAllConstraints(x._2, constrints))
-        if (tempListOfNodeId.length > count)
-            Random.shuffle(tempListOfNodeId).take(count).map(x => x._1)
-        else
-            tempListOfNodeId.map(x => x._1)
+        tempListOfNodeId.map(x => x._1)
     }
 
     def doesAllCapablitiesMatchAllConstraints(cap: List[Constraint], con: List[Constraint]): Boolean = {
-        ???
+        con.foldLeft(true)((x, y) => doesCapablitiesMatchTheConstraint(cap, y) && x)
     }
 
-    def canFindProperResourcesForTheseConstraints(constraints: List[Constraint]): Boolean = true
+    def doesCapablitiesMatchTheConstraint(cap: List[Constraint], con: Constraint): Boolean = {
+        val temp = cap.filter(x => x.name == con.name)
+        temp match {
+            case List() => false
+            case _      => doseCapabilityMatchConstraint(temp(0), con)
+        }
+    }
 
-    def whichTaskShouldSubmittedToThisNode(waveNumber: Int, resource: Resource, nodeId: NodeId): Option[List[TaskDescription]] = None
+    def doseCapabilityMatchConstraint(cap: Constraint, con: Constraint) = con.operator match {
+        case 0 => if (cap.value == con.value) true else false
+        case 1 => if (cap.value != con.value) true else false
+        case 2 => if (cap.value < con.value) true else false
+        case 3 => if (cap.value > con.value) true else false
+    }
+
+    def whichTaskShouldSubmittedToThisNode(lastWave: Int, resource: Resource, nodeId: NodeId): Option[List[TaskDescription]] = {
+        val unscheduledTasks = getUnscheduledTasks(completedWave + 1, List())
+        var result: Option[List[TaskDescription]] = None
+
+        if (unscheduledTasks == List()) {
+            completedWave = lastWave
+            result = None
+        }
+        else {
+            if (nodeIdToTaskIndex.get(nodeId) != None) {
+                val unscheduledTasksIndexesForThisNodeId = nodeIdToTaskIndex.get(nodeId).get.filter(x => unscheduledTasks.exists(y => y.index == x))
+                if (unscheduledTasksIndexesForThisNodeId.length > 0) {
+                    val unscheduledTasksIndexSortedBasedOnLowestPossibleOptions = nodeIdToTaskIndex.toList.map(x => x._2).flatten.filter(x => unscheduledTasksIndexesForThisNodeId.contains(x)).groupBy(x => x).toList.sortWith((x, y) => x._2.length < y._2.length).map(x => x._1)
+                    val temp = getBestTaskTachCanFitThisResource(resource, unscheduledTasks, unscheduledTasksIndexSortedBasedOnLowestPossibleOptions, List())
+                    temp match {
+                        case List() => result = None
+                        case _      => result = Some(temp)
+                    }
+                }
+                else
+                    None //It's resource with constraint let preserver it
+                nodeIdToTaskIndex.remove(nodeId)
+            }
+            else {
+                val unscheduledTasksWithOutConstraint = unscheduledTasks.filter(x => x.constraints == null || x.constraints == List()).sortWith((x, y) => x.resource > y.resource)
+                //TODO:it can be knapstak 
+                val temp = getBigestTasksThatCanFitThisResource(resource, unscheduledTasksWithOutConstraint, List())
+                temp match {
+                    case List() => result = None
+                    case _      => result = Some(temp)
+                }
+            }
+        }
+        result
+    }
+
+    def getBestTaskTachCanFitThisResource(resource: Resource, tasks: List[TaskDescription], order: List[Int], result: List[TaskDescription]): List[TaskDescription] = order match {
+        case List() => result
+        case x :: xs => {
+            val taskDescTemp = tasks.find(y => y.index == x)
+            taskDescTemp match {
+                case None => getBestTaskTachCanFitThisResource(resource, tasks, xs, result)
+                case Some(x) => if (x.resource < resource)
+                    getBestTaskTachCanFitThisResource(resource - x.resource, tasks, xs, x :: result)
+                else
+                    getBestTaskTachCanFitThisResource(resource, tasks, xs, result)
+            }
+        }
+    }
+
+    @annotation.tailrec
+    def getBigestTasksThatCanFitThisResource(resource: Resource, tasks: List[TaskDescription], result: List[TaskDescription]): List[TaskDescription] = tasks match {
+        case List() => result
+        case x :: xs => if (x.resource < resource)
+            getBigestTasksThatCanFitThisResource(resource - x.resource, xs, x :: result)
+        else
+            getBigestTasksThatCanFitThisResource(resource, xs, result)
+    }
+
+    def getUnscheduledTasks(waveNumber: Int, tasks: List[TaskDescription]): List[TaskDescription] = waveToTasks.get(waveNumber) match {
+        case None    => tasks
+        case Some(x) => getUnscheduledTasks(waveNumber + 1, getUnscheduledTaskOfWave(waveNumber) ++ tasks)
+    }
 
     def getUnscheduledTaskOfWave(waveNumber: Int) = waveToTasks.get(waveNumber).get.filter(x => x._1 == false).map(y => y._2)
 
     def addNewSubmittedTasksIntoWaveToTaks(waveNumber: Int, tasks: List[TaskDescription]) = {
         waveToTasks.update(waveNumber, tasks.map(x => (false, x)))
     }
+
+    def canFindProperResourcesForTheseConstraints(constraints: List[Constraint]): Boolean = true
 }
